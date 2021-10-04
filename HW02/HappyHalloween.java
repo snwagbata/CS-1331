d06fc019295a939e63066d94176e6f757ac35b46
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-10-04 14:45:32 -0400

    class testing

--- HW02/HappyHalloween.java
+++ HW02/HappyHalloween.java
@@ -1,3 +1,31 @@
+/**
+ * This file defines the class HappyHalloween.
+ * @author Somtochukwu Nwagbata
+ * @version 1.0
+ */
 public class HappyHalloween {
-    
+    /**
+     * This method prints the Happy Halloween message.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BlackCat blackCat = new BlackCat("Pussy", false);
+        BlackCat blackCat2 = new BlackCat("Meow", true);
+        Witch witch = new Witch("Shark", "Eat", blackCat);
+        Witch witch2 = new Witch(witch);
+        witch2.setName("Sharky");
+        witch.getCompanion().setName("Meow");
+        System.out.println(witch2.getCompanion().getName());
+        Ghost ghost = new Ghost("Ghosty", "Scary", 69, 9);
+        Ghost ghost2 = new Ghost(ghost);
+        ghost.seekCandy(420);
+        ghost.setTransparency(1);
+        ghost2.spook();
+        ghost.seekCandy(7);
+        ghost2.seekCandy(420);
+        witch.seekCandy(69);
+        System.out.println(ghost2.getNumCandy());
+        System.out.println(witch.getNumCandy());
+        System.out.println(TrickOrTreater.getTotalCandy());
+    }
 }
