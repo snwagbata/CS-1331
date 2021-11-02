a854ab21c8cf51f243ce57a7e8171399e9e1c01f
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-11-02 15:52:49 -0400

    finished InvalidBookingException class

--- HW06/InvalidBookingException.java
+++ HW06/InvalidBookingException.java
@@ -1,3 +1,21 @@
-public class InvalidBookingException {
-    
+/**
+ * This class describes a checked exception.
+ * @author Somtochukwu Nwagbata
+ * @version 1.0
+ */
+public class InvalidBookingException extends Exception {
+    /**
+     * 1-arg constructor that takes a message.
+     * @param message the message to be displayed.
+     */
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+
+    /**
+     * No-arg constructor.
+     */
+    public InvalidBookingException() {
+        super("Invalid booking type");
+    }
 }
