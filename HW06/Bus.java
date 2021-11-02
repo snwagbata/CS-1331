00f5f4384edb0781d881ba62e16c8646c16968c7
Somtochukwu Nwagbata <healer2030@gmail.com>
2021-11-02 15:53:33 -0400

    finished flight and bus classes

--- HW06/Bus.java
+++ HW06/Bus.java
@@ -1,3 +1,49 @@
-public class Bus {
-    
+/**
+ * This class describes a bus, and it is a concrete implementation of Transportation.
+ * @author Somtochukwu Nwagbata
+ * @version 1.0
+ */
+public class Bus extends Transportation {
+    private int stops;
+
+    /**
+     * This is the constructor for the Bus class.
+     * @param company The company that owns the bus.
+     * @param id The id of the bus.
+     * @param departDate The date the bus departs.
+     * @param departTime The time the bus departs.
+     * @param arrivalTime The time the bus arrives.
+     * @param stops The number of stops the bus makes.
+     */
+    public Bus(String company, int id, String departDate, String departTime, String arrivalTime, int stops) {
+        super(company, id, departDate, departTime, arrivalTime);
+        if (stops < 0) {
+            throw new IllegalArgumentException("Stops cannot be negative");
+        } else {
+            this.stops = stops;
+        }
+    }
+
+    /**
+     * Equals method for the Bus class.
+     * @param obj The object to compare to.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Bus) {
+            Bus other = (Bus) obj;
+            return super.equals(other) && this.stops == other.stops;
+        }
+        return false;
+    }
+
+    /**
+     * Returns a string representation of the bus.
+     * @return A string representation of the bus.
+     */
+    @Override
+    public String toString() {
+        return String.format("Bus,%s,%d", super.toString(), this.stops);
+    }
 }
--- HW06/Flight.java
+++ HW06/Flight.java
@@ -1,3 +1,51 @@
-public class Flight {
-    
+/**
+ * This class describes a flight, and it is a concrete implementation of Transportation.
+ * @author Somtochukwu Nwagbata
+ * @version 1.0
+ */
+public class Flight extends Transportation {
+    private int connectors;
+
+    /**
+     * Constructor for objects of class Flight.
+     * @param company The company that is providing the flight.
+     * @param id The id of the flight.
+     * @param departDate The date of departure.
+     * @param departTime The time of departure.
+     * @param arrivalTime The time of arrival.
+     * @param connectors The number of connectors.
+     * @throws IllegalArgumentException if the connectors is less than 0.
+     */
+    public Flight(String company, int id, String departDate, String departTime, String arrivalTime, int connectors) {
+        super(company, id, departDate, departTime, arrivalTime);
+
+        if (connectors < 0) {
+            throw new IllegalArgumentException("Connectors must be greater than 0");
+        } else {
+            this.connectors = connectors;
+        }
+    }
+
+    /**
+     * Equals method for Flight objects.
+     * @param obj The other Flight object to compare to.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Flight) {
+            Flight other = (Flight) obj;
+            return super.equals(other) && connectors == other.connectors;
+        }
+        return false;
+    }
+
+    /**
+     * Returns a string representation of the Flight object.
+     * @return A string representation of the Flight object.
+     */
+    @Override
+    public String toString() {
+        return String.format("Flight,%s,%d", super.toString(), connectors);
+    }
 }
